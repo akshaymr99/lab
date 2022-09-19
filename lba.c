27230a2b90a241b58ba5098956c81888dbe249f0
Akshay mr <70700183+amr6969@users.noreply.github.com>
2022-09-19 07:55:07 +0530

    Add files via upload

---
+++ lba.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+    int incoming, outgoing, buck_size, n, store = 0;
+    printf("Enter bucket size : ");
+    scanf("%d", &buck_size);
+    printf("Enter outgoing rate : ");
+    scanf("%d", &outgoing);
+    printf("Enter no of inputs : "); 
+    scanf("%d", &n);
+    printf("\n----------------------------------------------------------------------------\n");
+
+    while (n != 0) {
+        printf("Enter the incoming packet size : ");
+        scanf("%d", &incoming);
+        printf("Incoming packet size %d\n", incoming);
+        if (incoming <= (buck_size - store)){
+            store += incoming;
+            printf("Bucket buffer size %d out of %d\n", store, buck_size);
+        } else {
+            printf("Dropped %d no of packets\n", incoming - (buck_size - store));
+            printf("Bucket buffer size %d out of %d\n", store, buck_size);
+            store = buck_size;
+        }
+        store = store - outgoing;
+        printf("After outgoing %d packets left out of %d in buffer\n", store, buck_size);
+        printf("\n----------------------------------------------------------------------------\n");
+        n--;
+    }
+}
